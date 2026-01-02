61d80291852f9fcd250f1f31bfddf88324920c8b
Weellyyy <wildanprtm2304@gmail.com>
2026-01-02 15:01:30 +0700

    menambahkan route dan argument navigasi halaman tambah

---
+++ app/src/main/java/com/example/myfirebase/view/route/DestinasiEntry.kt
@@ -0,0 +1,8 @@
+package com.example.myfirebase.view.route
+import com.example.myfirebase.R
+import com.example.myfirebase.view.route.DestinasiNavigasi
+
+object DestinasiEntry: DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
